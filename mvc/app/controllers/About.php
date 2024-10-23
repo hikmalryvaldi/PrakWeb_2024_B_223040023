feba40b0d0e921bd3bbd76381d53503ef8f4e5a1
hikmalryvaldi <hikmalryvaldi@gmail.com>
2024-10-23 17:32:24 +0700

    Video 5 - View

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,14 +1,23 @@
 <?php
 
-class About
+class About extends Controller
 {
 
-  public function index($nama = "Hikmal", $pekerjaan = "Mahasiswa")
+  public function index($nama = "Hikmal", $pekerjaan = "Mahasiswa", $umur = 21)
   {
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+    $this->view("templates/header", $data);
+    $this->view("about/index", $data);
+    $this->view("templates/footer");
   }
   public function page()
   {
-    echo "About/page";
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
